51d4a944b668d307ffc0e2ba7e73d0a403448c73
Quainter2510 <nihgpavel@yandex.ru>
2023-09-20 00:04:14 +0300

    update counting of points

--- loader.py
+++ loader.py
@@ -2,6 +2,6 @@ from telebot import TeleBot, apihelper
 from config_data import config
 from database.common import MyDataBase
 
-apihelper.SESSION_TIME_TO_LIVE = 5 * 60
+# apihelper.SESSION_TIME_TO_LIVE = 5 * 60
 bot = TeleBot(token=config.BOT_TOKEN)
 base = MyDataBase()
